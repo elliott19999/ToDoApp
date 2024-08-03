25e7b4b0bf3278f00415275bc3dbf4690b3f2169
elliott <elliott>
2024-08-03 18:59:35 +0300

    no message

---
+++ ToDoApp/ToDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Мадина Валиева on 03.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+       
+        }
+   
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Мадина Валиева on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
